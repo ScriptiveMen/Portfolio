494764752ec96a4e959a0d2007d3a3439bf85a31
ScriptiveMen <i.satyakumarram@gmail.com>
2025-07-31 19:44:45 +0530

    fixed loader

--- script.js
+++ script.js
@@ -31,12 +31,26 @@ tl.from(".child>span", {
   })
   .to(".parent .child", {
     y: "-100%",
-    duration: 1,
+    duration: 0.8,
     ease: Circ.easeInOut,
   })
   .to(".loader", {
     height: 0,
-    duration: 1.5,
-    delay: -0.7,
-    ease: Circ.easeInOut,
+    duration: 1,
+    delay: 0.4,
+    ease: "power4.out",
+  })
+  .to(".green", {
+    height: "100%",
+    top: 0,
+    duration: 1.2,
+    delay: -1.2,
+    ease: "power4.out",
+  })
+  .to(".green", {
+    height: "0%",
+    top: 0,
+    duration: 1.1,
+    delay: -1,
+    ease: "power4.out",
   });
