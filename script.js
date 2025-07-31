06a49b9dc271ff487e5029a1bf0e1d4a5c0dc503
ScriptiveMen <i.satyakumarram@gmail.com>
2025-08-01 02:11:29 +0530

    hero1

--- script.js
+++ script.js
@@ -14,50 +14,62 @@ function spanArchitecture() {
     elem.appendChild(spanParent);
   });
 }
-spanArchitecture();
 
-let xValue;
-if (window.innerWidth <= 767) {
-  xValue = 40; // Mobile ke liye
-} else {
-  xValue = 100; // PC ke liye
+function loaderAnimation() {
+  let xValue;
+  if (window.innerWidth <= 767) {
+    xValue = 40; // Mobile ke liye
+  } else {
+    xValue = 100; // PC ke liye
+  }
+
+  let tl = gsap.timeline();
+
+  tl.from(".child>span", {
+    x: xValue,
+    duration: 0.7,
+    stagger: 0.1,
+    ease: Power3.easeInOut,
+  })
+    .from(".child>span:nth-child(2), .child>span:last-child", {
+      opacity: 0,
+      duration: 0.7,
+      delay: -0.5,
+    })
+    .to(".parent .child", {
+      y: "-100%",
+      duration: 0.8,
+      ease: Circ.easeInOut,
+    })
+    .to(".loader", {
+      height: 0,
+      duration: 1,
+      delay: 0.4,
+      ease: "power4.out",
+    })
+    .to(".green", {
+      height: "100%",
+      top: 0,
+      duration: 1.2,
+      delay: -1.2,
+      ease: "power4.out",
+    })
+    .to(".green", {
+      height: "0%",
+      top: 0,
+      duration: 1.1,
+      delay: -1,
+      ease: "power4.out",
+    });
 }
 
-let tl = gsap.timeline();
+function date() {
+  let dt = new Date();
 
-tl.from(".child>span", {
-  x: xValue,
-  duration: 0.7,
-  stagger: 0.1,
-  ease: Power3.easeInOut,
-})
-  .from(".child>span:nth-child(2), .child>span:last-child", {
-    opacity: 0,
-    duration: 0.7,
-    delay: -0.5,
-  })
-  .to(".parent .child", {
-    y: "-100%",
-    duration: 0.8,
-    ease: Circ.easeInOut,
-  })
-  .to(".loader", {
-    height: 0,
-    duration: 1,
-    delay: 0.4,
-    ease: "power4.out",
-  })
-  .to(".green", {
-    height: "100%",
-    top: 0,
-    duration: 1.2,
-    delay: -1.2,
-    ease: "power4.out",
-  })
-  .to(".green", {
-    height: "0%",
-    top: 0,
-    duration: 1.1,
-    delay: -1,
-    ease: "power4.out",
-  });
+  let newTime = `${dt.getHours()}:${dt.getMinutes()}`;
+  document.querySelector(".time").textContent = newTime;
+}
+
+spanArchitecture();
+loaderAnimation();
+date();
